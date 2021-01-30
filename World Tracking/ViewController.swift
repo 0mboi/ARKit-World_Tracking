a918d743d0c2d37b95c5c73f0217365f9814a7ef
Christopher Hicks <visrule@protonmail.com>
2021-01-29 17:13:56 -0800

    Init of World Tracking
    Learnig ARKit and Git

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
+    
+    let configuration = ARWorldTrackingConfiguration()
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // shows us if was properly executed - debug
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
     }
 
 
